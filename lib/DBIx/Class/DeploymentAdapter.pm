bd1ead9ba14b127ec4b076d5e052d9a11070b386
Patrick Kilter <pas.freelance@googlemail.com>
2016-09-01 11:13:01 +0200

    Checking in changes prior to tagging of version 0.03.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 2d09c40..b1c7558 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension DBIx-Class-DeploymentAdapter
    
     {{$NEXT}}
    
    +0.03 2016-09-01T09:13:00Z
    +
    +    - deployment handler args per new()
    +
     0.02 2016-09-01T09:04:28Z
    
         - mysqldump backup WIP

--- lib/DBIx/Class/DeploymentAdapter.pm
+++ lib/DBIx/Class/DeploymentAdapter.pm
@@ -3,7 +3,7 @@ use 5.008001;
 use strict;
 use warnings;
 
-our $VERSION = "0.02";
+our $VERSION = "0.03";
 
 use DBIx::Class::DeploymentHandler;
 
@@ -44,7 +44,7 @@ around BUILDARGS => sub {
     }
 
     return $class->$orig(@_);
-}
+};
 
 sub install {
 
