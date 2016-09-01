506637a3aca08d0cf6fcbcca308bea142d92eb7b
Patrick Kilter <pas.freelance@googlemail.com>
2016-09-01 09:49:12 +0200

    added storage handling refs #4078

--- lib/DBIx/Class/DeploymentAdapter.pm
+++ lib/DBIx/Class/DeploymentAdapter.pm
@@ -3,4 +3,32 @@ use 5.008001;
 use strict;
 use warnings;
 
+use DBIx::Class::DeploymentHandler;
+
+use Moose;
+
+has dh_store => (
+    is  => "rw",
+    isa => "Maybe[Object]"
+);
+
+sub dh {
+
+    my ( $self, $args ) = @_;
+
+    if ( !$self->dh_store ) {
+
+        return unless $args && $args->{schema};
+
+        $args->{script_directory}    ||= "./share/migrations";
+        $args->{databases}           ||= ["MySQL"];
+        $args->{sql_translator_args} ||= { mysql_enable_utf8 => 1 };
+
+        $self->dh_store( DBIx::Class::DeploymentHandler->new( $args ) );
+
+    }
+
+    return $self->dh_store;
+}
+
 1;
