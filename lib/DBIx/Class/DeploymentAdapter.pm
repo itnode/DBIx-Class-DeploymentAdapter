2d7e2c12f0c02bac75fd22d5e38de3012d1a4710
Patrick Kilter <pas.freelance@googlemail.com>
2016-09-01 09:59:55 +0200

    added functions

--- lib/DBIx/Class/DeploymentAdapter.pm
+++ lib/DBIx/Class/DeploymentAdapter.pm
@@ -24,11 +24,98 @@ sub dh {
         $args->{databases}           ||= ["MySQL"];
         $args->{sql_translator_args} ||= { mysql_enable_utf8 => 1 };
 
-        $self->dh_store( DBIx::Class::DeploymentHandler->new( $args ) );
+        $self->dh_store( DBIx::Class::DeploymentHandler->new($args) );
 
     }
 
     return $self->dh_store;
+}
+
+sub install {
+
+    my ( $self ) = @_;
+
+    return unless $self->dh;
+
+    $self->dh->install;
+}
+
+sub prepare {
+
+    my ($self) = @_;
+
+    return unless $self->dh;
+
+    my $start_version  = $self->dh->database_version;
+    my $target_version = $self->dh->schema->schema_version;
+
+    $dh->prepare_install;
+
+    $dh->prepare_upgrade(
+        {
+            from_version => $start_version,
+            to_version   => $target_version,
+        }
+    );
+
+    $dh->prepare_downgrade(
+        {
+            from_version => $target_version,
+            to_version   => $start_version,
+        }
+    );
+}
+
+sub status {
+
+    my ( $self ) = @_;
+
+    return unless $self->dh;
+
+    my $deployed_version = $self->dh->database_version;
+    my $schema_version   = $self->dh->schema->schema_version;
+
+    return sprintf( "Schema is %s\nDeployed database is %s\n", $schema_version, $deployed_version );
+
+}
+
+sub upgrade_incremental {
+
+    my ( $self ) = @_;
+
+    return unless $self->dh;
+
+    my $start_version  = $self->dh->database_version;
+    my $target_version = $self->dh->schema->schema_version;
+
+    my $start_version  = $self->dh->database_version + 1;
+    my $target_version = $schema->schema_version;
+
+    for my $version ( $start_version .. $target_version ) {
+
+        if( $options->{to_version} && $version >= $options->{to_version} ) {
+            next;
+        }
+
+        warn "upgrading to version $version\n";
+
+        eval {
+            my ( $ddl, $sql ) = @{ $self->dh->upgrade_single_step( { version_set => [ $version - 1, $version ] } ) || [] };    # from last version to desired version
+            $self->dh->add_database_version(
+                {
+                    version     => $version,
+                    ddl         => $ddl,
+                    upgrade_sql => $sql,
+                }
+            );
+        };
+
+        if ($@) {
+            my $error_version = $self->dh->database_version;
+            warn "Database remains on version $error_version";
+            die "UPGRADE ERROR - Version $error_version upgrading to $version: " . $@;
+        }
+    }
 }
 
 1;
